49f6b425379d56a0b400e5cd4ce1877f034ba462
roragaruda <153317816+roragaruda@users.noreply.github.com>
2025-02-01 04:35:51 +0700

    Add files via upload

--- config/dbConnect.go
+++ config/dbConnect.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"log"
 	"os"
 
 	"gorm.io/driver/postgres"
@@ -11,10 +12,17 @@ var DB *gorm.DB
 
 func ConnectDB() {
 	db_url := os.Getenv("DB_URL")
+	if db_url == "" {
+		log.Fatal("Environment variable DB_URL is not set")
+	}
+
+	log.Println("Connecting to database:", db_url) // Debugging log
+
 	dbConn, err := gorm.Open(postgres.Open(db_url), &gorm.Config{})
 	if err != nil {
-		panic("Failed connecting to Database")
+		log.Fatal("Failed to connect to Database:", err)
 	}
 
 	DB = dbConn
+	log.Println("Database connected successfully")
 }
