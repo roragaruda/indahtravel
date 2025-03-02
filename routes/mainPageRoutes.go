d0adc189805781236dfde0f8cfc7c7ecacdaadd9
roragaruda <153317816+roragaruda@users.noreply.github.com>
2025-03-02 15:29:28 +0700

    Add files via upload

--- routes/mainPageRoutes.go
+++ routes/mainPageRoutes.go
@@ -6,14 +6,18 @@ import (
 )
 
 func MainPageRoutes(r *gin.Engine) {
-	r.GET("/", controllers.Index)
-	r.GET("/katalog", controllers.Katalog)
-	r.GET("/gallery", controllers.GalleryPage)
-	r.GET("/aboutus", controllers.AboutUs)
-	r.GET("/unauthorized", controllers.Unauthorized)
-	r.GET("/detail/:id", controllers.DetailPage)
-	r.GET("/gallery/:id", controllers.GalleryDetail)
-	r.GET("/order/:id", controllers.OrderPage)
+	indahwisata := r.Group("/indahwisata") // Prefix "indahwisata"
+	{
+		indahwisata.GET("/", controllers.Index)
+		indahwisata.GET("/katalog", controllers.Katalog)
+		indahwisata.GET("/gallery", controllers.GalleryPage)
+		indahwisata.GET("/aboutus", controllers.AboutUs)
+		indahwisata.GET("/unauthorized", controllers.Unauthorized)
+		indahwisata.GET("/detail/:id", controllers.DetailPage)
+		indahwisata.GET("/gallery/:id", controllers.GalleryDetail)
+		indahwisata.GET("/order/:id", controllers.OrderPage)
+	}
+
 	r.GET("/header.html", controllers.Header)
 	r.GET("/footer.html", controllers.Footer)
 	r.GET("/tatacara.html", controllers.Tatacara)
