07a047bc5ef89a0b3ed0fdb2add0dbb71ce2ea23
roragaruda <153317816+roragaruda@users.noreply.github.com>
2025-03-02 15:00:34 +0700

    Add files via upload

--- routes/mainPageRoutes.go
+++ routes/mainPageRoutes.go
@@ -6,19 +6,19 @@ import (
 )
 
 func MainPageRoutes(r *gin.Engine) {
-	// Redirect dari root ke /indahwisata
+	// Redirect dari root ke /indahwisata dengan path absolut
 	r.GET("/", func(c *gin.Context) {
 		c.Redirect(302, "/indahwisata")
 	})
 
 	r.GET("/indahwisata", controllers.Index)
-	r.GET("/indahwisata/katalog", controllers.Katalog)
-	r.GET("/indahwisata/gallery", controllers.GalleryPage)
-	r.GET("/indahwisata/aboutus", controllers.AboutUs)
-	r.GET("/indahwisata/unauthorized", controllers.Unauthorized)
-	r.GET("/indahwisata/detail/:id", controllers.DetailPage)
-	r.GET("/indahwisata/gallery/:id", controllers.GalleryDetail)
-	r.GET("/indahwisata/order/:id", controllers.OrderPage)
+	r.GET("/katalog", controllers.Katalog)
+	r.GET("/gallery", controllers.GalleryPage)
+	r.GET("/aboutus", controllers.AboutUs)
+	r.GET("/unauthorized", controllers.Unauthorized)
+	r.GET("/detail/:id", controllers.DetailPage)
+	r.GET("/gallery/:id", controllers.GalleryDetail)
+	r.GET("/order/:id", controllers.OrderPage)
 	r.GET("/header.html", controllers.Header)
 	r.GET("/footer.html", controllers.Footer)
 	r.GET("/tatacara.html", controllers.Tatacara)
