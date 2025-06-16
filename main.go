d36e014b8788fbf0701d0c81481d5c16b7380333
roragaruda <153317816+roragaruda@users.noreply.github.com>
2025-06-16 09:14:01 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"os"
 	"github.com/attanabilrabbani/indahwisatabe/config"
 	"github.com/attanabilrabbani/indahwisatabe/routes"
 	"github.com/gin-gonic/gin"
-	"os"
 )
 
 func init() {
@@ -24,11 +24,9 @@ func main() {
 	routes.AdminPageRoutes(r)
 	routes.MainPageRoutes(r)
 
-	// ambil PORT dari env, default 8080 kalau kosong (buat local dev)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-
 	r.Run(":" + port)
 }
